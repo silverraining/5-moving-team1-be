b45dc52b8097052a70c67a53251c86732e689c26
민지영 <kuck523@gmail.com>
2025-05-23 09:49:35 +0900

    fix: eslint 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,7 +30,10 @@ export default tseslint.config(
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/no-explicit-any': 'off', // 여기 확실히 꺼짐
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
