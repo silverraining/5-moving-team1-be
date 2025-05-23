b6e4cc6d636b11ecb17cbe3f560df3bd42239a23
민지영 <kuck523@gmail.com>
2025-05-23 13:30:32 +0900

    feat: create-user dto 유효성 추가

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -3,32 +3,41 @@ import {
   IsEnum,
   IsOptional,
   IsString,
-  MinLength,
+  Length,
+  Matches,
 } from 'class-validator';
-import { Provider, Role } from '../entities/user.entity';
+import { Role } from '../entities/user.entity';
 
 export class CreateUserDto {
   @IsEnum(Role)
   role: Role;
 
   @IsString()
+  @Length(2, 20, { message: '이름은 2자 이상 20자 이하이어야 합니다.' })
+  @Matches(/^[가-힣a-zA-Z]/, {
+    message:
+      '이름은 한글 또는 영어만 가능하며, 공백과 특수문자는 사용할 수 없습니다.',
+  })
   name: string;
 
   @IsString()
+  @Matches(/^010\d{8}$/, {
+    message: '휴대폰 번호는 010으로 시작하는 11자리 숫자여야 합니다.',
+  })
   phone: string;
 
-  @IsEmail()
+  @IsEmail({}, { message: '유효한 이메일 주소를 입력해주세요.' })
   email: string;
 
   @IsOptional()
   @IsString()
-  @MinLength(8)
+  @Length(8, 20, { message: '비밀번호는 8자 이상 20자 이하로 입력해주세요.' })
+  @Matches(
+    /^(?!.*\s)(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,20}$/,
+    {
+      message:
+        '비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 하며 공백 없이 입력해야 합니다.',
+    },
+  )
   password?: string;
-
-  @IsEnum(Provider)
-  provider: Provider;
-
-  @IsOptional()
-  @IsString()
-  snsId?: string;
 }
