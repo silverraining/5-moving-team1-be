9292d99cc446f7a807dc937ae97e8a6a576bdd1e
민지영 <kuck523@gmail.com>
2025-05-24 01:41:35 +0900

    fix: PassportModul imports에 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalStrategy } from './strategy/local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), JwtModule.register({})],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.register({}),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService, JwtModule],
