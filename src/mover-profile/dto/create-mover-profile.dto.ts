3a631eebc0123ff5939ffaf0f46e952bc3fcef78
민지영 <kuck523@gmail.com>
2025-05-27 14:58:09 +0900

    refactor:validator 설정

--- src/mover-profile/dto/create-mover-profile.dto.ts
+++ src/mover-profile/dto/create-mover-profile.dto.ts
@@ -1,11 +1,26 @@
-import { IsInt, IsObject, IsOptional, IsString } from 'class-validator';
+import {
+  IsInt,
+  IsObject,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+  Max,
+  Min,
+} from 'class-validator';
 import {
   ServiceRegionMap,
   ServiceTypeMap,
 } from 'src/common/const/service.const';
+import { HasAtLeastOneTrue } from 'src/common/validator/service.validator';
 
 export class CreateMoverProfileDto {
   @IsString()
+  @Length(2, 20, { message: '닉네임은 2자 이상 20자 이하여야 합니다.' })
+  @Matches(/^[가-힣a-zA-Z0-9]+$/, {
+    message:
+      '닉네임은 한글, 영문, 숫자만 사용할 수 있으며 공백 및 특수문자는 허용되지 않습니다.',
+  })
   nickname: string;
 
   @IsOptional()
@@ -13,17 +28,30 @@ export class CreateMoverProfileDto {
   imageUrl?: string;
 
   @IsInt()
+  @Min(0, { message: '경력은 최소 0이상 입력해야 합니다.' })
+  @Max(99, { message: '경력은 최대 99까지 입력해야 합니다.' })
   experience: number;
 
   @IsString()
+  @Length(8, 50, { message: '소개는 8자 이상 50자 이하 입니다.' })
+  @Matches(/^[^\r\n]*$/, {
+    message: '소개에는 줄바꿈을 포함할 수 없습니다.',
+  })
   intro: string;
 
   @IsString()
+  @Length(10, 500, { message: '설명은 10자 이상 500자 이하 입니다.' })
   description: string;
 
   @IsObject()
+  @HasAtLeastOneTrue({
+    message: '서비스 타입은 최소 하나 이상 선택되어야 합니다.',
+  })
   serviceType: ServiceTypeMap;
 
   @IsObject()
+  @HasAtLeastOneTrue({
+    message: '서비스 지역은 최소 하나 이상 선택되어야 합니다.',
+  })
   serviceRegion: ServiceRegionMap;
 }
