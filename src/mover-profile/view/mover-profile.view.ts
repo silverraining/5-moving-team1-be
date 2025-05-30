91a0a1f9f55f9028d71f81b7ac119703c9d5e5f2
민지영 <kuck523@gmail.com>
2025-05-30 15:09:27 +0900

    feat: mover profile view table

---
+++ src/mover-profile/view/mover-profile.view.ts
@@ -0,0 +1,36 @@
+import { Review } from '@/review/entities/review.entity';
+import { DataSource, ViewColumn, ViewEntity } from 'typeorm';
+import { MoverProfile } from '../entities/mover-profile.entity';
+import { EstimateOffer } from '@/estimate-offer/entities/estimate-offer.entity';
+
+@ViewEntity({
+  name: 'mover_profile_view',
+  expression: (dataSource: DataSource) =>
+    dataSource
+      .createQueryBuilder()
+      .select('mover.id', 'id')
+      .addSelect('COUNT(DISTINCT review.id)', 'review_count')
+      .addSelect('AVG(review.rating)', 'average_rating')
+      .addSelect('COUNT(DISTINCT estimate_offer.id)', 'estimate_offer_count')
+      .from(MoverProfile, 'mover')
+      .leftJoin(Review, 'review', 'review.mover_id = mover.id')
+      .leftJoin(
+        EstimateOffer,
+        'estimate_offer',
+        'estimate_offer.mover_id = mover.id',
+      )
+      .groupBy('mover.id'),
+})
+export class MoverProfileView {
+  @ViewColumn()
+  id: string;
+
+  @ViewColumn()
+  review_count: number;
+
+  @ViewColumn()
+  average_rating: number;
+
+  @ViewColumn()
+  estimate_offer_count: number;
+}
