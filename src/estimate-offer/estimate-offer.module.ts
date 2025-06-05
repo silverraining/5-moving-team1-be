0149c3913e3951446b71c6ee005e59c541b6df71
silverraining <www.stella.choi@gmail.com>
2025-06-05 14:43:37 +0900

    feat: EstimateOfferModule에 EstimateRequest 의존성 추가

--- src/estimate-offer/estimate-offer.module.ts
+++ src/estimate-offer/estimate-offer.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EstimateOfferService } from './estimate-offer.service';
 import { EstimateOfferController } from './estimate-offer.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EstimateOffer } from './entities/estimate-offer.entity';
+import { EstimateRequest } from '@/estimate-request/entities/estimate-request.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([EstimateOffer, EstimateRequest])],
   controllers: [EstimateOfferController],
   providers: [EstimateOfferService],
+  exports: [EstimateOfferService],
 })
 export class EstimateOfferModule {}
