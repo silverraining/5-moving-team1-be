fb593634f11c0deb3ef386e646182a4ffa325191
silverraining <www.stella.choi@gmail.com>
2025-06-06 05:33:12 +0900

    refactor: 견적 조회 응답 dto 다시 정의

--- src/estimate-offer/dto/estimate-offer-response.dto.ts
+++ src/estimate-offer/dto/estimate-offer-response.dto.ts
@@ -1,23 +1,77 @@
-import { ServiceRegion, ServiceTypeMap } from '@/common/const/service.const';
+import { Expose, Transform } from 'class-transformer';
+import {
+  ServiceRegion,
+  ServiceType,
+  ServiceTypeMap,
+} from '@/common/const/service.const';
 import { OfferStatus } from '../entities/estimate-offer.entity';
+import { RequestStatus } from '@/estimate-request/entities/estimate-request.entity';
+
+type MinimalAddress = {
+  sido: string;
+  sigungu: string;
+  roadAddress?: string; // 상세페이지
+};
 
 export class EstimateOfferResponseDto {
+  @Expose()
   estimateRequestId: string;
+
+  @Expose()
   moverId: string;
+
+  @Expose()
   price: number;
-  comment?: string;
+
+  @Expose()
   status: OfferStatus;
+
+  @Expose()
+  requestStatus: RequestStatus; // 견적 요청 상태 (PENDING, COMPLETED 등)
+
+  @Expose()
   isTargeted: boolean;
-  isConfirmed: boolean;
+
+  @Expose()
+  isConfirmed: boolean; // 견적 제안이 확정되었는지
+
+  @Expose()
+  confirmedCount: number; //기사님 카드 확정건수
+
+  @Expose()
   confirmedAt: Date;
 
-  // mover 프로필 응답에 포함
+  @Expose()
+  moveDate: Date; // 상세페이지에 '이용일'
+  @Expose()
+  moveType: ServiceType; // 상세페이지 '서비스'
+
+  @Expose()
+  createdAt: Date; // 상세페이지에 견적 요청일
+
+  // fromAddress → { sido, sigungu, roadAddress }만 추출
+  @Transform(({ obj }) => ({
+    sido: obj.fromAddress?.sido,
+    sigungu: obj.fromAddress?.sigungu,
+    roadAddress: obj.fromAddress?.roadAddress,
+  }))
+  @Expose()
+  fromAddress: MinimalAddress;
+
+  @Transform(({ obj }) => ({
+    sido: obj.toAddress?.sido,
+    sigungu: obj.toAddress?.sigungu,
+    roadAddress: obj.toAddress?.roadAddress,
+  }))
+  @Expose()
+  toAddress: MinimalAddress;
+
+  @Expose()
   mover: {
     nickname: string;
     imageUrl?: string;
     experience: number;
-    serviceType: ServiceTypeMap;
-    serviceRegion: ServiceRegion;
+    serviceType: ServiceTypeMap; //제공 서비스 타입 (기사님 카드에 chip)
     intro: string;
     rating: number;
     reviewCount: number;
