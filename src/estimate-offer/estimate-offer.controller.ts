c2ead01be9ea6798424213601a9895c2754576d6
silverraining <www.stella.choi@gmail.com>
2025-06-05 15:44:13 +0900

    feat: 스웨거 인증을 위한 ApiBearerAuth 데코레이터 추가

--- src/estimate-offer/estimate-offer.controller.ts
+++ src/estimate-offer/estimate-offer.controller.ts
@@ -12,12 +12,15 @@ import { EstimateOfferService } from './estimate-offer.service';
 import { CreateEstimateOfferDto } from './dto/create-estimate-offer.dto';
 import { UpdateEstimateOfferDto } from './dto/update-estimate-offer.dto';
 import { UserInfo } from '@/user/decorator/user-info.decorator';
-
+import { ApiGetEstimateOffers } from './docs/swagger';
+import { ApiBearerAuth } from '@nestjs/swagger';
 @Controller('estimate-offer')
 export class EstimateOfferController {
   constructor(private readonly estimateOfferService: EstimateOfferService) {}
 
   @Get()
+  @ApiBearerAuth()
+  @ApiGetEstimateOffers()
   async getOffersByEstimateRequestId(
     @Query('id') estimateRequestId: string,
     @UserInfo() userInfo: UserInfo, // 현재 로그인한 유저 정보
@@ -30,11 +33,6 @@ export class EstimateOfferController {
   @Post()
   create(@Body() createEstimateOfferDto: CreateEstimateOfferDto) {
     return this.estimateOfferService.create(createEstimateOfferDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.estimateOfferService.findAll();
   }
 
   @Patch(':id')
