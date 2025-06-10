7c13592c528a94c5f917b9ddbcd251131351ff96
민지영 <kuck523@gmail.com>
2025-06-10 13:42:41 +0900

    refactor: customer profile 업데이트 수정 (사용자 정보 수정), 조회 수정 (조회 내용에 사용자 정보 추가)

--- src/customer-profile/customer-profile.module.ts
+++ src/customer-profile/customer-profile.module.ts
@@ -4,9 +4,14 @@ import { CustomerProfileController } from './customer-profile.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from '@/common/common.module';
 import { CustomerProfile } from './entities/customer-profile.entity';
+import { UserModule } from '@/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CustomerProfile]), CommonModule],
+  imports: [
+    TypeOrmModule.forFeature([CustomerProfile]),
+    UserModule,
+    CommonModule,
+  ],
   controllers: [CustomerProfileController],
   providers: [CustomerProfileService],
 })
--- src/customer-profile/customer-profile.service.ts
+++ src/customer-profile/customer-profile.service.ts
@@ -8,12 +8,15 @@ import { UpdateCustomerProfileDto } from './dto/update-customer-profile.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CustomerProfile } from './entities/customer-profile.entity';
 import { Repository } from 'typeorm';
+import { UserService } from '@/user/user.service';
+import { UpdateUserDto } from '@/user/dto/update-user.dto';
 
 @Injectable()
 export class CustomerProfileService {
   constructor(
     @InjectRepository(CustomerProfile)
     private readonly customerProfileRepository: Repository<CustomerProfile>,
+    private readonly userService: UserService,
   ) {}
 
   async create(
@@ -36,40 +39,65 @@ export class CustomerProfileService {
     return newProfile;
   }
 
-  findOne(userId: string) {
-    const profile = this.customerProfileRepository.findOneBy({
-      user: { id: userId },
+  async findOne(userId: string) {
+    const profile = await this.customerProfileRepository.findOne({
+      where: { user: { id: userId } },
+      relations: ['user'],
     });
 
     if (!profile) {
       throw new NotFoundException('고객님의 프로필을 찾을 수 없습니다!');
     }
 
-    return profile;
+    const { user, ...restProfileData } = profile;
+    const { name, email, phone } = user; // 사용자 정보 가져오기
+
+    return {
+      name,
+      email,
+      phone,
+      ...restProfileData,
+    };
   }
 
-  async update(
-    userId: string,
-    updateCustomerProfileDto: UpdateCustomerProfileDto,
-  ) {
+  async update(userId: string, profileDto: UpdateCustomerProfileDto) {
+    const { name, phone, password, newPassword, ...updatedProfileData } =
+      profileDto;
+
+    // 사용자 정보 업데이트를 위한 객체 생성
+    const updatedUserData: UpdateUserDto = {
+      name,
+      phone,
+      password,
+      newPassword,
+    };
+
+    // 사용자 정보 업데이트
+    await this.userService.update(userId, updatedUserData); // 반환값 무시
+
+    // 프로필 정보 조회
     const profile = await this.customerProfileRepository.findOneBy({
       user: { id: userId },
     });
 
+    // 프로필 존재 여부 확인
     if (!profile) {
       throw new NotFoundException('고객님의 프로필을 찾을 수 없습니다!');
     }
 
-    Object.assign(profile, updateCustomerProfileDto);
-
+    // 프로필 정보 업데이트
+    Object.assign(profile, updatedProfileData);
     const updatedProfile = await this.customerProfileRepository.save(profile);
 
+    // 업데이트 실패 시 예외 처리
     if (!updatedProfile) {
       throw new InternalServerErrorException(
         '고객님의 프로필 업데이트에 실패했습니다!',
       );
     }
 
-    return updatedProfile;
+    return {
+      message: '고객님의 프로필이 성공적으로 업데이트되었습니다.',
+    };
   }
 }
