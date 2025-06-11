c912ba8dc791f373ce702758c7ec1906b6fd9b9d
민지영 <kuck523@gmail.com>
2025-06-11 11:42:15 +0900

    refactor: role을 query가 아닌 body로 받는 것으로 수정, localAuthGuard에서 dto로 검증 수행 추가

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -4,6 +4,9 @@ import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
 import { Request } from 'express';
 import { Provider, Role } from '@/user/entities/user.entity';
+import { validateSync, ValidationError } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+import { LocalLoginDto } from '../dto/local-login.dto';
 
 export class LocalAuthGuard extends AuthGuard('local') {}
 
@@ -25,12 +28,21 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
    */
   async validate(req: Request, email: string, password: string) {
     // role 유효성 검사
-    const role = req.query.state as Role;
-    if (!role || !Object.values(Role).includes(role)) {
-      throw new BadRequestException('유효하지 않은 역할이 제공되었습니다.');
+    const { role } = req.body as { role: Role }; // body에서 role 추출
+
+    // DTO 인스턴스 생성
+    const dto = plainToInstance(LocalLoginDto, { email, password, role });
+
+    // dto 검증 수행
+    const errors = validateSync(dto);
+
+    // 모든 에러메시지를 배열로 반환
+    if (errors.length > 0) {
+      const allMessages = this.getAllErrorMessages(errors);
+      throw new BadRequestException(allMessages);
     }
 
-    // 사용자 조회
+    // 사용자 인증
     const user = await this.authService.authenticate(
       role,
       Provider.LOCAL,
@@ -46,5 +58,28 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     };
 
     return payload;
+  }
+
+  /**
+   * 모든 ValidationError 메시지를 가져오는 함수
+   * 재귀적으로 모든 에러 메시지를 수집합니다.
+   *
+   * @param errors
+   * @returns
+   */
+
+  private getAllErrorMessages(errors: ValidationError[]): string[] {
+    let messages: string[] = [];
+
+    for (const err of errors) {
+      if (err.constraints) {
+        messages.push(...Object.values(err.constraints));
+      }
+
+      if (err.children && err.children.length) {
+        messages = messages.concat(this.getAllErrorMessages(err.children));
+      }
+    }
+    return messages;
   }
 }
