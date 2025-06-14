93cb1bf7ee45a7bc9f8ba54d53c0b421c6ad1621
민지영 <kuck523@gmail.com>
2025-06-14 21:55:13 +0900

    refactor: 사용자 정보 수정 기능 응답 코드 리팩토링

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Patch } from '@nestjs/common';
+import { Controller, Body, Patch, HttpCode } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserInfo } from './decorator/user-info.decorator';
@@ -10,6 +10,7 @@ export class UserController {
 
   //User 기본정보 수정 API
   @Patch('me')
+  @HttpCode(204) // 204 No Content 응답을 위해 HttpCode 데코레이터 사용
   @ApiUpdateMe()
   async update(
     @UserInfo() userInfo: UserInfo,
