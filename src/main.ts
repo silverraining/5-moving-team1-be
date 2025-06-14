38c101a4f1e88dc43b79b8542d2eef0030ac13aa
hay-oon <hayooncode@gmail.com>
2025-06-14 13:55:00 +0900

    fix: 배포 오류 해결시도

--- src/main.ts
+++ src/main.ts
@@ -51,7 +51,7 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`
  🚀 서버 실행 완료!                                         
