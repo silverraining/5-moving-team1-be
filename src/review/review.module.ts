491114432a7c326b0c637766f40b1823aeeea32d
민지영 <kuck523@gmail.com>
2025-06-17 14:07:05 +0900

    feat: review api (작업중)

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from './entities/review.entity';
+import { EstimateOffer } from '@/estimate-offer/entities/estimate-offer.entity';
+import { EstimateRequest } from '@/estimate-request/entities/estimate-request.entity';
+import { CommonModule } from '@/common/common.module';
+import { CustomerProfileModule } from '@/customer-profile/customer-profile.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Review, EstimateRequest, EstimateOffer]),
+    CommonModule,
+    CustomerProfileModule,
+  ],
   controllers: [ReviewController],
   providers: [ReviewService],
 })
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,26 +1,151 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
-import { UpdateReviewDto } from './dto/update-review.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Review } from './entities/review.entity';
+import { EstimateOffer } from '@/estimate-offer/entities/estimate-offer.entity';
+import {
+  EstimateRequest,
+  RequestStatus,
+} from '@/estimate-request/entities/estimate-request.entity';
+import { PagePaginationDto } from '@/common/dto/page-pagination.dto';
+import { CommonService } from '@/common/common.service';
+import { CustomerProfileService } from '@/customer-profile/customer-profile.service';
 
 @Injectable()
 export class ReviewService {
-  create(createReviewDto: CreateReviewDto) {
-    return 'This action adds a new review';
+  constructor(
+    @InjectRepository(Review)
+    private readonly reviewRepository: Repository<Review>,
+    @InjectRepository(EstimateRequest)
+    private readonly estimateRequestRepository: Repository<EstimateRequest>,
+    @InjectRepository(EstimateOffer)
+    private readonly estimateOfferRepository: Repository<EstimateOffer>,
+
+    private readonly commonService: CommonService,
+    private readonly customerProfileService: CustomerProfileService,
+  ) {}
+
+  /**
+   *
+   * 리뷰 작성 서비스
+   *
+   * @param userId
+   * @param confirmedOfferId
+   * @param createReviewDto
+   * @returns { message: string }
+   */
+
+  async create(
+    userId: string,
+    confirmedOfferId: string,
+    createReviewDto: CreateReviewDto,
+  ) {
+    // 1. 사용자 ID로 고객 ID를 추출
+    const customerId = await this.customerProfileService.getCustomerId(userId);
+
+    // 2. 견적 제안 ID로 해당 견적 제안을 조회
+    const offer = await this.estimateOfferRepository.findOne({
+      where: { id: confirmedOfferId },
+      select: ['moverId'],
+    });
+
+    if (!offer) {
+      throw new NotFoundException('해당 견적 제안을 찾을 수 없습니다.');
+    }
+
+    const { moverId } = offer; // 견적 제안에서 기사 ID 추출
+
+    // 3. 이미 리뷰가 존재하는지 확인
+    const existing = await this.reviewRepository.findOne({
+      where: {
+        estimateOfferId: confirmedOfferId,
+        customerId,
+      },
+    });
+
+    if (existing) {
+      throw new ConflictException('이미 리뷰를 작성하셨습니다.');
+    }
+
+    // 4. 리뷰 데이터 생성
+    const reviewData = {
+      estimateOfferId: confirmedOfferId,
+      customerId,
+      moverId,
+      ...createReviewDto,
+    };
+
+    await this.reviewRepository.save(reviewData); // 리뷰 저장
+
+    return { message: '리뷰가 성공적으로 작성되었습니다.' };
   }
 
-  findAll() {
-    return `This action returns all review`;
+  /**
+   *  작성 가능한 리뷰 목록 조회
+   *
+   *  페이지네이션
+   *
+   *  @param userId - 사용자 ID
+   *  @returns reviewableOffers: 작성 가능한 리뷰 목록
+   *  @returns total: 페이지네이션을 위한 총 개수
+   */
+
+  async findAllAvailable(userId: string, dto: PagePaginationDto) {
+    const qb = this.estimateRequestRepository
+      .createQueryBuilder('request')
+      .leftJoin('request.customer', 'customer')
+      .leftJoin('customer.user', 'user')
+      .leftJoin(EstimateOffer, 'offer', 'offer.id = request.confirmedOfferId')
+      .leftJoin('offer.mover', 'mover')
+      .where('user.id = :userId', { userId }) // 사용자 ID로 필터링
+      .andWhere('request.status = :status', { status: RequestStatus.COMPLETED }) // 완료 요청 상태 필터링
+      .andWhere('request.confirmedOfferId IS NOT NULL') // confirmedOfferId가 있는 요청만 선택
+      .andWhere('review.estimateOfferId IS NULL') // 리뷰가 없는 견적 제안만 선택
+      .select([
+        'offer.id AS confirmedOfferId',
+        'request.moveType AS moveType',
+        'request.moveDate AS moveDate',
+        'offer.price AS price',
+        'offer.isTargeted AS isTargeted',
+        'mover.nickname AS moverNickname',
+        'mover.imageUrl AS moverImageUrl',
+      ]);
+
+    this.commonService.applyPagePaginationParamsToQb(qb, dto); // 페이지네이션 적용
+
+    const rawReviewableOffers = await qb.getRawMany();
+    const total = await qb.getCount();
+
+    const formattedReviewableOffers = rawReviewableOffers.map((row) => ({
+      reviewableOfferId: row.confirmedOfferId, // 작성 가능한 리뷰 견적 제안 ID
+      moveType: row.moveType, // 이사 종류
+      moveDate: row.moveDate, // 이사일
+      price: row.price, // 확정된 제안의 가격
+      isTargeted: row.isTargeted, // 확정된 제안의 지정견적 여부
+      mover: {
+        nickname: row.moverNickname, // 기사의 닉네임
+        imageUrl: row.moverImageUrl, // 기사의 이미지 URL
+      },
+    }));
+
+    return {
+      reviewableOffers: formattedReviewableOffers, // 작성 가능한 리뷰 목록
+      total, // 페이지네이션을 위한 총 개수
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} review`;
+  findByCustomerId(customerId: string) {
+    return this.reviewRepository.find({
+      where: { customerId },
+    });
   }
 
-  update(id: number, updateReviewDto: UpdateReviewDto) {
-    return `This action updates a #${id} review`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} review`;
+  findByMoverId(moverId: string) {
+    return `This action returns a #${moverId} review`;
   }
 }
