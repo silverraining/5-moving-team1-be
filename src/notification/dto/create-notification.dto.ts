1adfb67764183845a9b43148a0da88257394fb9f
김조순 <whtns611@naver.com>
2025-06-17 11:15:43 +0900

    feat : Dto 추가

--- src/notification/dto/create-notification.dto.ts
+++ src/notification/dto/create-notification.dto.ts
@@ -1,1 +1,16 @@
-export class CreateNotificationDto {}
+import { IsEnum, IsString, IsUUID } from 'class-validator';
+import { NotificationType } from '../entities/notification.entity';
+
+export class CreateNotificationDto {
+  @IsEnum(NotificationType)
+  type: NotificationType;
+
+  @IsString()
+  message: string;
+
+  @IsString()
+  targetId: string;
+
+  @IsUUID()
+  userId: string;
+}
