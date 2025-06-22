422c1016fede2dd7cddb107785c6a32e8d3eec9a
김조순 <whtns611@naver.com>
2025-06-22 23:15:21 +0900

    feat : 신규 알림 생성 이벤트 추가

--- src/notification/events/dispatcher.ts
+++ src/notification/events/dispatcher.ts
@@ -1,6 +1,18 @@
 import { EventEmitter2 } from '@nestjs/event-emitter';
-import { TargetMoverUpdatedEvent } from './event';
+import {
+  NewReviewEvent,
+  OfferComfirmUpdateEvent,
+  TargetMoverUpdatedEvent,
+  TargetOfferUpdateEvent,
+} from './event';
 import { Injectable } from '@nestjs/common';
+
+/**
+ * 지정 견적 생성 리스너
+ * moverId : 지정한 mover의 id
+ * ps.profile id여도 상관 없음
+ * requestId : customer가 진행중인 request id
+ */
 
 @Injectable()
 export class EstimateRequestEventDispatcher {
@@ -10,6 +22,55 @@ export class EstimateRequestEventDispatcher {
     this.eventEmitter.emit(
       'estimate-request.target-mover-updated',
       new TargetMoverUpdatedEvent(requestId, moverId),
+    );
+  }
+}
+
+/**
+ * 새로운 견적 생성 리스너
+ * customerId : customer의 id profile id 여도 상관 없음
+ * offerId : mover가 생성한 견적서의 id
+ */
+@Injectable()
+export class NewEstimateOfferEventDispatcher {
+  constructor(private readonly eventEmitter: EventEmitter2) {}
+
+  targetMoverAssigned(customerId: string, offerId: string) {
+    this.eventEmitter.emit(
+      'new-estimate-orffer.target-customer-updated',
+      new TargetOfferUpdateEvent(customerId, offerId),
+    );
+  }
+}
+
+/**
+ * 견적 확정 알림
+ * moverId : 무버 id
+ * offerId : 견적서 id
+ */
+@Injectable()
+export class OfferConfirmEventDispatcher {
+  constructor(private readonly eventEmitter: EventEmitter2) {}
+
+  targetMoverAssigned(moverId: string, offerId: string) {
+    this.eventEmitter.emit(
+      'confirm-estimate-orffer.target-mover-updated',
+      new OfferComfirmUpdateEvent(moverId, offerId),
+    );
+  }
+}
+/**
+ * 신규 리뷰 등록 이벤트
+ */
+
+@Injectable()
+export class NewReviewEventDispatcher {
+  constructor(private readonly eventEmitter: EventEmitter2) {}
+
+  targetMoverAssigned(moverId: string, reviewId: string) {
+    this.eventEmitter.emit(
+      'new-review.target-mover-updated',
+      new NewReviewEvent(moverId, reviewId),
     );
   }
 }
--- src/notification/listeners/listener.ts
+++ src/notification/listeners/listener.ts
@@ -1,22 +1,27 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
-import { TargetMoverUpdatedEvent } from '../events/event';
+import {
+  NewReviewEvent,
+  OfferComfirmUpdateEvent,
+  TargetMoverUpdatedEvent,
+  TargetOfferUpdateEvent,
+} from '../events/event';
 import { NotificationService } from 'src/notification/notification.service';
 import { NotificationType } from '../entities/notification.entity';
 
+/**
+ * 지정 견적 생성 리스너
+ * moverId : 지정한 mover의 id
+ * ps.profile id여도 상관 없음
+ * requestId : customer가 진행중인 request id
+ */
 @Injectable()
 export class EstimateRequestListener {
-  private readonly logger = new Logger(EstimateRequestListener.name);
-
   constructor(private readonly notificationService: NotificationService) {}
 
   @OnEvent('estimate-request.target-mover-updated')
   async handleTargetMoverUpdated(event: TargetMoverUpdatedEvent) {
     const { moverId, requestId } = event;
-
-    this.logger.log(
-      `기사 ID: ${moverId} 가 요청 ID: ${requestId} 에 지정되었습니다.`,
-    );
 
     // 알림 저장
     await this.notificationService.create({
@@ -24,6 +29,73 @@ export class EstimateRequestListener {
       type: NotificationType.NEW_ESTIMATE_REQUEST,
       message: `새로운 견적 요청이 도착했습니다.`,
       targetId: requestId,
+    });
+  }
+}
+
+/**
+ * 새로운 견적 생성 리스너
+ * customerId : customer의 id profile id 여도 상관 없음
+ * offerId : mover가 생성한 견적서의 id
+ */
+@Injectable()
+export class NewEstimateOfferListener {
+  constructor(private readonly notificationService: NotificationService) {}
+
+  @OnEvent('new-estimate-orffer.target-customer-updated')
+  async handleTargetMoverUpdated(event: TargetOfferUpdateEvent) {
+    const { customerId, offerId } = event;
+
+    // 알림 저장
+    await this.notificationService.create({
+      userId: customerId,
+      type: NotificationType.NEW_OFFER,
+      message: `새로운 견적이 도착했습니다.`,
+      targetId: offerId,
+    });
+  }
+}
+
+/**
+ * 견적 확정 알림
+ * moverId : 무버 id
+ * offerId : 견적서 id
+ */
+
+@Injectable()
+export class OfferConfirmListener {
+  constructor(private readonly notificationService: NotificationService) {}
+
+  @OnEvent('confirm-estimate-orffer.target-mover-updated')
+  async handleTargetMoverUpdated(event: OfferComfirmUpdateEvent) {
+    const { moverId, offerId } = event;
+
+    // 알림 저장
+    await this.notificationService.create({
+      userId: moverId,
+      type: NotificationType.ESTIMATE_CONFIRMED,
+      message: `견적이 확정 되었습니다.`,
+      targetId: offerId,
+    });
+  }
+}
+/**
+ * 신규 리뷰 알림
+ */
+@Injectable()
+export class NewReviewListener {
+  constructor(private readonly notificationService: NotificationService) {}
+
+  @OnEvent('create-reveiw.target-mover-updated')
+  async handleTargetMoverUpdated(event: NewReviewEvent) {
+    const { moverId, reveiwId } = event;
+
+    // 알림 저장
+    await this.notificationService.create({
+      userId: moverId,
+      type: NotificationType.CREATE_REVIEW,
+      message: `신규 리뷰가 있습니다.`,
+      targetId: reveiwId,
     });
   }
 }
